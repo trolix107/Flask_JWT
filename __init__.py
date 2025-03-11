5ec0c601d98ecf301178be441d44d7abb45ac40b
trolix107 <gregoire.barix@edu.esiee-it.fr>
2025-03-11 13:46:36 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
-                                                                                                                                       
+                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
 # Configuration du module JWT
